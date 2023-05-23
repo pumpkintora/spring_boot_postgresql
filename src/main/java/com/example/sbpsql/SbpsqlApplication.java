b828f1ca86aed597071ebaf3bd5a35eeb9570b47
pumpkintora <renkiat.gan@corpxervices.com>
2023-05-23 13:25:21 +0800

    first commit

---
+++ src/main/java/com/example/sbpsql/SbpsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbpsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbpsql/SbpsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbpsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbpsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
